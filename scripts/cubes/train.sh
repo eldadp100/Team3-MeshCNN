2c1a80be1eaadd4744304f0df63179579c1c48a8
Eldad Peretz <eldadperetz@mail.tau.ac.il>
2020-11-30 09:22:56 +0200

    Update train.sh

--- scripts/cubes/train.sh
+++ scripts/cubes/train.sh
@@ -4,7 +4,10 @@
 python train.py \
 --dataroot datasets/cubes \
 --name cubes \
---arch mesh_transformer \
+--ncf 64 128 256 256 \
+--pool_res 600 450 300 210 \
+--norm group \
+--resblocks 1 \
+--flip_edges 0.2 \
 --slide_verts 0.2 \
 --num_aug 20 \
---lr 0.0003
